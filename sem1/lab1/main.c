aa99676610a1eb1203d59732a7d69be1310b0cc9
Ivan Shtarev <ishtarev@outlook.com>
2023-11-15 00:07:18 +0300

    fix last name and middle name bug

--- sem1/lab1/main.c
+++ sem1/lab1/main.c
@@ -3,19 +3,19 @@
 #include <string.h>
 
 #define DB_LEN 10
-#define MAX_FULLNAME_DB 51
-#define MAX_LASTNAME_DB 51
-#define MAX_FIRSTNAME_DB 51
-#define MAX_MIDDLENAME_DB 51
+#define MAX_NAME_DB 51
 #define MAX_NORMALIZEDPHONE_DB 12
 #define MAX_PRETTYPHONE_DB 19
 #define MAX_PHONE_INPUT 100
 
 struct Contact {
-        char fullName[MAX_FULLNAME_DB];
-        char lastName[MAX_LASTNAME_DB];
-        char firstName[MAX_FIRSTNAME_DB];
-        char middleName[MAX_MIDDLENAME_DB];
+        char name0[MAX_NAME_DB];
+        char name1[MAX_NAME_DB];
+        char name2[MAX_NAME_DB];
+        char name3[MAX_NAME_DB];
+        char name4[MAX_NAME_DB];
+        char name5[MAX_NAME_DB];
+        char name6[MAX_NAME_DB];
         char normalizedPhone[MAX_NORMALIZEDPHONE_DB];
         char prettyPhone[MAX_PRETTYPHONE_DB];
     };
@@ -23,73 +23,103 @@ struct Contact {
 struct Contact contacts[DB_LEN];
 
 void dbFill() {
-    strcpy(contacts[0].fullName, "Ivanov Ivan Ivanovich");
-    strcpy(contacts[0].fullName, "Ivanov Ivan Ivanovich");
-    strcpy(contacts[0].fullName, "Ivanov Ivan Ivanovich");
-    strcpy(contacts[0].fullName, "Ivanov Ivan Ivanovich");
+    strcpy(contacts[0].name0, "Ivanov Ivan Ivanovich");
+    strcpy(contacts[0].name1, "Ivanov Ivan");
+    strcpy(contacts[0].name2, "Ivan Ivanovich");
+    strcpy(contacts[0].name3, "Ivanov Ivanovich");
+    strcpy(contacts[0].name4, "Ivanov");
+    strcpy(contacts[0].name5, "Ivan");
+    strcpy(contacts[0].name6, "Ivanovich");
     strcpy(contacts[0].normalizedPhone, "79818883322");
     strcpy(contacts[0].prettyPhone, "+7 (981) 888-33-22");
 
-    strcpy(contacts[1].fullName, "Kirillov Anton Efimovich");
-    strcpy(contacts[1].fullName, "Kirillov Anton Efimovich");
-    strcpy(contacts[1].fullName, "Kirillov Anton Efimovich");
-    strcpy(contacts[1].fullName, "Kirillov Anton Efimovich");
+    strcpy(contacts[1].name0, "Kirillov Anton Efimovich");
+    strcpy(contacts[1].name1, "Kirillov Anton");
+    strcpy(contacts[1].name2, "Anton Efimovich");
+    strcpy(contacts[1].name3, "Kirillov Efimovich");
+    strcpy(contacts[1].name4, "Kirillov");
+    strcpy(contacts[1].name5, "Anton");
+    strcpy(contacts[1].name6, "Efimovich");
     strcpy(contacts[1].normalizedPhone, "79998881100");
     strcpy(contacts[1].prettyPhone, "+7 (999) 888-11-00");
     
-    strcpy(contacts[2].fullName, "Afanasieva Olga");
-    strcpy(contacts[2].fullName, "Afanasieva Olga");
-    strcpy(contacts[2].fullName, "Afanasieva Olga");
-    strcpy(contacts[2].fullName, "Afanasieva Olga");
+    strcpy(contacts[2].name0, "Afanasieva Olga");
+    strcpy(contacts[2].name1, "");
+    strcpy(contacts[2].name2, "");
+    strcpy(contacts[2].name3, "");
+    strcpy(contacts[2].name4, "Afanasieva");
+    strcpy(contacts[2].name5, "Olga");
+    strcpy(contacts[2].name6, "");
     strcpy(contacts[2].normalizedPhone, "79813451122");
     strcpy(contacts[2].prettyPhone, "+7 (981) 345-11-22");
 
-    strcpy(contacts[3].fullName, "Lermontov Mikhail Yurievich");
-    strcpy(contacts[3].fullName, "Lermontov Mikhail Yurievich");
-    strcpy(contacts[3].fullName, "Lermontov Mikhail Yurievich");
-    strcpy(contacts[3].fullName, "Lermontov Mikhail Yurievich");
+    strcpy(contacts[3].name0, "Lermontov Mikhail Yurievich");
+    strcpy(contacts[3].name1, "Lermontov Mikhail");
+    strcpy(contacts[3].name2, "Mikhail Yurievich");
+    strcpy(contacts[3].name3, "Lermontov Yurievich");
+    strcpy(contacts[3].name4, "Lermontov");
+    strcpy(contacts[3].name5, "Mikhail");
+    strcpy(contacts[3].name6, "Yurievich");
     strcpy(contacts[3].normalizedPhone, "79008765431");
     strcpy(contacts[3].prettyPhone, "8 (900) 876-54-31");
 
-    strcpy(contacts[4].fullName, "Pushkin Alexander Sergeevich");
-    strcpy(contacts[4].fullName, "Pushkin Alexander Sergeevich");
-    strcpy(contacts[4].fullName, "Pushkin Alexander Sergeevich");
-    strcpy(contacts[4].fullName, "Pushkin Alexander Sergeevich");
+    strcpy(contacts[4].name0, "Pushkin Alexander Sergeevich");
+    strcpy(contacts[4].name1, "Pushkin Alexander");
+    strcpy(contacts[4].name2, "Alexander Sergeevich");
+    strcpy(contacts[4].name3, "Pushkin Sergeevich");
+    strcpy(contacts[4].name4, "Pushkin");
+    strcpy(contacts[4].name5, "Alexander");
+    strcpy(contacts[4].name6, "Sergeevich");
     strcpy(contacts[4].normalizedPhone, "71119876521");
     strcpy(contacts[4].prettyPhone, "+7 (111) 987-65-21");
 
-    strcpy(contacts[5].fullName, "Fet Afanasy Afanasyevich");
-    strcpy(contacts[5].fullName, "Fet Afanasy Afanasyevich");
-    strcpy(contacts[5].fullName, "Fet Afanasy Afanasyevich");
-    strcpy(contacts[5].fullName, "Fet Afanasy Afanasyevich");
+    strcpy(contacts[5].name0, "Fet Afanasy Afanasyevich");
+    strcpy(contacts[5].name1, "Fet Afanasy");
+    strcpy(contacts[5].name2, "Afanasy Afanasyevich");
+    strcpy(contacts[5].name3, "Fet Afanasyevich");
+    strcpy(contacts[5].name4, "Fet");
+    strcpy(contacts[5].name5, "Afanasy");
+    strcpy(contacts[5].name6, "Afanasyevich");
     strcpy(contacts[5].normalizedPhone, "70002345434");
     strcpy(contacts[5].prettyPhone, "8 (000) 234-54-34");
 
-    strcpy(contacts[6].fullName, "Euclid");
-    strcpy(contacts[6].fullName, "Euclid");
-    strcpy(contacts[6].fullName, "Euclid");
-    strcpy(contacts[6].fullName, "Euclid");
+    strcpy(contacts[6].name0, "Euclid");
+    strcpy(contacts[6].name1, "");
+    strcpy(contacts[6].name2, "");
+    strcpy(contacts[6].name3, "");
+    strcpy(contacts[6].name4, "");
+    strcpy(contacts[6].name5, "Euclid");
+    strcpy(contacts[6].name6, "");
     strcpy(contacts[6].normalizedPhone, "71234567899");
     strcpy(contacts[6].prettyPhone, "+7 (123) 456-78-99");
     
-    strcpy(contacts[7].fullName, "H.G. Tannhaus");
-    strcpy(contacts[7].fullName, "H.G. Tannhaus");
-    strcpy(contacts[7].fullName, "H.G. Tannhaus");
-    strcpy(contacts[7].fullName, "H.G. Tannhaus");
+    strcpy(contacts[7].name0, "Tannhaus H.G.");
+    strcpy(contacts[7].name1, "");
+    strcpy(contacts[7].name2, "");
+    strcpy(contacts[7].name3, "");
+    strcpy(contacts[7].name4, "Tannhaus");
+    strcpy(contacts[7].name5, "H.G.");
+    strcpy(contacts[7].name6, "");
     strcpy(contacts[7].normalizedPhone, "73333333333");
     strcpy(contacts[7].prettyPhone, "+7 (333) 333-33-33");
     
-    strcpy(contacts[8].fullName, "Marlon Brando");
-    strcpy(contacts[8].fullName, "Marlon Brando");
-    strcpy(contacts[8].fullName, "Marlon Brando");
-    strcpy(contacts[8].fullName, "Marlon Brando");
+    strcpy(contacts[8].name0, "Brando Marlon");
+    strcpy(contacts[8].name1, "");
+    strcpy(contacts[8].name2, "");
+    strcpy(contacts[8].name3, "");
+    strcpy(contacts[8].name4, "Brando");
+    strcpy(contacts[8].name5, "Marlon");
+    strcpy(contacts[8].name6, "");
     strcpy(contacts[8].normalizedPhone, "79826242638");
     strcpy(contacts[8].prettyPhone, "+7 (982) 624-26-38");
     
-    strcpy(contacts[9].fullName, "Roman Polanski");
-    strcpy(contacts[9].fullName, "Roman Polanski");
-    strcpy(contacts[9].fullName, "Roman Polanski");
-    strcpy(contacts[9].fullName, "Roman Polanski");
+    strcpy(contacts[9].name0, "Polanski Roman");
+    strcpy(contacts[9].name1, "");
+    strcpy(contacts[9].name2, "");
+    strcpy(contacts[9].name3, "");
+    strcpy(contacts[9].name4, "Polanski");
+    strcpy(contacts[9].name5, "Roman");
+    strcpy(contacts[9].name6, "");
     strcpy(contacts[9].normalizedPhone, "71053536523");
     strcpy(contacts[9].prettyPhone, "+7 (105) 353-65-23");
 }
@@ -134,7 +164,7 @@ int isStringOnlyDigits(const char *str) {
 int main() {
     dbFill();
 
-    const char charsToRemove[] = " \t\n+-()";
+    const char charsToRemove[] = "+\n";
     char inputString[MAX_PHONE_INPUT];
     short int lookforNumber = 0;
     short int recordFound = 0;
@@ -146,16 +176,22 @@ int main() {
         lookforNumber = 1;
     }
 
-    for (int i = 0; i <= 5; i++) {
+    for (int i = 0; i < DB_LEN; i++) {
         if (lookforNumber == 1) {
             if (strcmp(contacts[i].normalizedPhone, inputString) == 0) {
-                printf("Found, %s, %s.\n", contacts[i].fullName, contacts[i].prettyPhone);
+                printf("Found, %s, %s.\n", contacts[i].name0, contacts[i].prettyPhone);
                 recordFound = 1;
                 break;
             }
         } else {
-            if (strcmp(contacts[i].fullName, inputString) == 0) {
-                printf("Found, %s, %s.\n", contacts[i].fullName, contacts[i].prettyPhone);
+            if ((strstr(contacts[i].name0, inputString) != NULL && strcmp(contacts[i].name0, "") != 0) ||
+                (strstr(contacts[i].name1, inputString) != NULL && strcmp(contacts[i].name1, "") != 0) ||
+                (strstr(contacts[i].name2, inputString) != NULL && strcmp(contacts[i].name2, "") != 0) ||
+                (strstr(contacts[i].name3, inputString) != NULL && strcmp(contacts[i].name3, "") != 0) ||
+                (strstr(contacts[i].name4, inputString) != NULL && strcmp(contacts[i].name4, "") != 0) ||
+                (strstr(contacts[i].name5, inputString) != NULL && strcmp(contacts[i].name5, "") != 0) ||
+                (strstr(contacts[i].name6, inputString) != NULL && strcmp(contacts[i].name6, "") != 0)) {
+                printf("Found, %s, %s.\n", contacts[i].name0, contacts[i].prettyPhone);
                 recordFound = 1;
                 break;
             }
